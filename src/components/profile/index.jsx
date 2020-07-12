9d88e5747706892b5e8d8f4e4827195f33173fc6
Kretawiweka Nuraga Sani <kretawiweka@Kretawiwekas-MacBook-Air.local>
2020-07-12 22:19:27 +0700

    Update typo and meta

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -15,9 +15,9 @@ const Profile = () => {
             <p className="description">
               As a software engineer, I want to pursue the whole field of
               software engineer. Even though in my career I have been focusing
-              more on the frontend engineer, but I also like to explore and
-              discuss whole field of software engineer and even like to discuss
-              business and design.
+              more on the frontend engineering, but I also like to explore and
+              discuss whole field of software engineering and even like to
+              discuss about business and design.
             </p>
           </div>
         </div>
